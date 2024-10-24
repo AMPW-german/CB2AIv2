9862e5c97fe0aa9e0fd0a73a8b33626d05ccffcf
AMPW-german <theampw@gmail.com>
2024-10-24 18:25:05 +0200

    testing the tracking

---
+++ video_track.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+
+model = YOLO(r"runs\\detect\\train\\weights\\best.pt")
+
+model.track(r"videos\\Testvideo_0.mov", imgsz = (900, 1600), visualize = False, save=True, conf=0.64)
