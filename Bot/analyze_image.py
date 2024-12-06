switching to bettercam (dxcam successor) fixing overflow/underflow errors in the image analyzer (image has dtype uint8, with the tolerance this can be exceded so now it has int16 as dtype. this ensures that no overflow/underflow can occur (at least with a reasonable tolerance))

Bot/analyze_image.py
@@ -56,9 +56,15 @@ def analyze_image(image_name, image_shape, image_count_name, fuel_percent_name, 
     pause = np.ndarray((1,), dtype=pause_dtype, buffer=pause_shm.buf)
     done = np.ndarray((1,), dtype=done_dtype, buffer=done_shm.buf)
 
+    img_cp_dtype = np.int16
+    img_cp = image.copy().astype(img_cp_dtype)
+    
+
     while 1:
         if image_count[0] > image_count_old:
             image_count_old = image_count[0]
+
+            img_cp = image.copy().astype(img_cp_dtype)
 
             # check fuel
             xStart = 262
@@ -67,7 +73,7 @@ def analyze_image(image_name, image_shape, image_count_name, fuel_percent_name, 
             yEnd = 20
             x_range = xEnd
             y_range = yEnd
-            count, fuel_percent[0] = check_pixels(image, xStart, yStart, x_range, y_range, fuel_list_2d)
+            count, fuel_percent[0] = check_pixels(img_cp, xStart, yStart, x_range, y_range, fuel_list_2d)
 
             # check health
             xStart = 441
@@ -76,7 +82,7 @@ def analyze_image(image_name, image_shape, image_count_name, fuel_percent_name, 
             yEnd = 22
             x_range = xEnd
             y_range = yEnd
-            count, health_percent[0] = check_pixels(image, xStart, yStart, x_range, y_range, health_list_2d)
+            count, health_percent[0] = check_pixels(img_cp, xStart, yStart, x_range, y_range, health_list_2d)
 
 
             # check base health
@@ -86,7 +92,7 @@ def analyze_image(image_name, image_shape, image_count_name, fuel_percent_name, 
             yEnd = 30
             x_range = xEnd
             y_range = yEnd
-            count, base_health_percent[0] = check_pixels(image, xStart, yStart, x_range, y_range, base_health_list_2d)
+            count, base_health_percent[0] = check_pixels(img_cp, xStart, yStart, x_range, y_range, base_health_list_2d)
 
             for list2check in points_of_interest:
                 counter = 0

Bot/video_tools.py
@@ -2,7 +2,7 @@ import pygetwindow as gw
 import numpy as np
 import time
 from multiprocessing import shared_memory
-import dxcam
+import bettercam
 
 left = 100
 top = 100
@@ -35,9 +35,6 @@ def timer(fps: int, image_name, image_shape, image_count_name, done_name):
     width = right - left
     height = bottom - top
 
-    print(window)
-
-
     image_dtype = np.uint8
     image_shm = shared_memory.SharedMemory(name=image_name)
     image = np.ndarray(image_shape, dtype=image_dtype, buffer=image_shm.buf)
@@ -49,7 +46,7 @@ def timer(fps: int, image_name, image_shape, image_count_name, done_name):
     done_shm = shared_memory.SharedMemory(name=done_name)
     done = np.ndarray((1,), dtype=done_dtype, buffer=done_shm.buf)
 
-    camera = dxcam.create(max_buffer_len=2)
+    camera = bettercam.create(max_buffer_len=2)
 
     camera.start(region=(left, top, right, bottom), target_fps=fps)
     # start = time.perf_counter()
@@ -87,9 +84,6 @@ def timer_fps(fps: int, image_name, image_shape, image_count_name, done_name):
     width = right - left
     height = bottom - top
 
-    print(window)
-
-
     image_dtype = np.uint8
     image_shm = shared_memory.SharedMemory(name=image_name)
     image = np.ndarray(image_shape, dtype=image_dtype, buffer=image_shm.buf)
@@ -101,7 +95,7 @@ def timer_fps(fps: int, image_name, image_shape, image_count_name, done_name):
     done_shm = shared_memory.SharedMemory(name=done_name)
     done = np.ndarray((1,), dtype=done_dtype, buffer=done_shm.buf)
 
-    camera = dxcam.create(max_buffer_len=2)
+    camera = bettercam.create(max_buffer_len=2)
 
     camera.start(region=(left, top, right, bottom), target_fps=fps)
     start = time.perf_counter()
@@ -120,7 +114,38 @@ def timer_fps(fps: int, image_name, image_shape, image_count_name, done_name):
 
         end_time = time.perf_counter() - start
         if end_time > 1:
-            print(f"DXC: {fps_count/end_time}")
+            print(f"bettercam: {fps_count/end_time}")
+            end_time = 0
+            fps_count = 0
+            start = time.perf_counter()
+
+
+if __name__ == "__main__":
+    program_name = "LDPlayer-2"
+    window = gw.getWindowsWithTitle(program_name)[0]  # Assumes the program is open and window is available
+    crop_area = {'left': 1, 'top': 34, 'right': 1, 'bottom': 1}  # Adjust as needed
+    left = window.left + crop_area['left']
+    top = window.top + crop_area['top']
+    right = window.left + window.width - crop_area['right']
+    bottom = window.top + window.height - crop_area['bottom']
+    width = right - left
+    height = bottom - top
+
+    camera = bettercam.create(max_buffer_len=2)
+
+    camera.start(region=(left, top, right, bottom), target_fps=80)
+    start = time.perf_counter()
+    fps_count = 0
+
+
+
+    while 1:
+        img = camera.get_latest_frame()
+        fps_count += 1
+
+        end_time = time.perf_counter() - start
+        if end_time > 1:
+            print(f"bettercam: {fps_count/end_time}")
             end_time = 0
             fps_count = 0
             start = time.perf_counter()